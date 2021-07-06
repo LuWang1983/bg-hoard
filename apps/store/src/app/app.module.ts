25e84a8fd87a7b8e74fdbd958bf42020a4164265
Lu Wang <loowang@hotmail.com>
2021-07-06 13:32:44 -0400

    add component lib and util lib

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -8,6 +9,7 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    StoreUiSharedModule,
     MatCardModule,
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
