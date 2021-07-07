23ba53d3c791a57b19e0eb92096e80f7dc86cea4
Lu Wang <loowang@hotmail.com>
2021-07-07 13:44:48 -0400

    Workspace generators Modifying files

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -2,7 +2,7 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 interface Schema {
   name: string;
-  directory: 'store' | 'api' | 'shared';
+  directory: 'api' | 'new' | 'store' | 'shared';
 }
 
 export default async function (host: Tree, schema: Schema) {
